06205986fbd6708e40a0b10cc21e924a6d4c7573
Ester <estertermensprat@iMac.home>
2019-04-09 20:07:28 +0200

    registro.js

--- web/js/custom.js
+++ web/js/custom.js
@@ -20,31 +20,75 @@ window.addEventListener('load', function(){
         
     });
 
+    //funcion para añadir clase segun estado
+        function claseNew(variable,nombreClass, text){
+            variable.classList.add(nombreClass);
+            variable.append(text);
+            event.preventDefault();
+        }
+
     //Validacion formulario de registro
-    var forValidation= document.querySelector('.needs-validation');
+        var forValidation= document.querySelector('.needs-validation');
+
+    
    
     forValidation.addEventListener('submit', function(event){
-       //capturo los datos
-       var name= document.querySelector('#nombre').value;
-       var surname= document.querySelector('#apellido').value;
-       var user= document.querySelector('#usuario').value;
-       var pass= document.querySelector('#contra').value;
-       var pass2= document.querySelector('#contraRep').value;
-       forValidation.classList.add('was-validated');//clase para dar los colores de boostrap
-       var validacion= document.querySelector('.validation');
+       
+        //capturo las variables
+            var name= forValidation.nombre.value;
+            var surname= forValidation.apellido.value;
+            var user= forValidation.usuario.value;
+            var pass= forValidation.contra.value;
+            var pass2= forValidation.contra2.value;
 
-       if(name === "null" || name.length <=0){  
-        validacion.classList.add('invalid-feeback');
-        validacion.append('Añadir nombre');
-        event.preventDefault();
-        
-        return false;
-       }else{
-        validacion.classList.add('valid-feeback');
-        validacion.append('Nombre correcto');
-        event.preventDefault();
-        return true;
+       forValidation.classList.add('was-validated');//clase para dar los colores de boostrap
+       
+       //Validar nombre
+       var validNombre= document.querySelector('.validNombre');
+       
+       if(name == "null" || name.length <=0){ 
+        claseNew(validNombre, 'invalid-feedback', 'Falta añadir el nombre');
+       }else{  
+        claseNew(validNombre, 'valid-feedback', 'El campo es correcto');
        }
+
+       //validar apellido
+       var validApellido= document.querySelector('.validApellido');
+       if(surname == "null" || surname.length <=0){ 
+        claseNew(validApellido, 'invalid-feedback', 'Falta añadir el apellido');
+       }else{  
+        claseNew(validApellido, 'valid-feedback', 'El campo es correcto');
+       }
+
+        //validar usuario
+        var validUser= document.querySelector('.validUser');
+        if(user == "null" || user.length <=0){ 
+         claseNew(validUser, 'invalid-feedback', 'Falta añadir el usuario');
+        }else{  
+         claseNew(validUser, 'valid-feedback', 'El usuario es correcto');
+        }
+
+        //validar contraseña
+        var validPass= document.querySelector('.validPass');
+        if(pass == "null" || pass.length <=0){ 
+         claseNew(validPass, 'invalid-feedback', 'Falta añadir el contraseña');
+        }else{  
+         claseNew(validPass, 'valid-feedback', 'El contraseña es correcto');
+        }
+
+        //validar contraseña2
+        if(pass2 !== pass){
+            alert('No coinciden las contraseñas');
+        }
+        var validPass2= document.querySelector('.validPass2');
+        if(pass2 == "null" || pass2.length <=0){ 
+         claseNew(validPass2, 'invalid-feedback', 'Contraseña incorrecta');
+        }else {
+         claseNew(validPass2, 'valid-feedback', 'El contraseña es correcta');
+        }
+       
+       
+       
       
        
     });
